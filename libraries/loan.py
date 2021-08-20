0cff0767881f49f9ce612763d0a8dd8acd71c9e5
Sangyups <feels4637@gmail.com>
2021-08-20 18:06:08 +0900

    feat: get available num of books

--- libraries/loan.py
+++ libraries/loan.py
@@ -24,9 +24,18 @@ def search_book_library(keyword):
   html = wd.page_source
   soup = BeautifulSoup(html, 'html.parser')
   items = soup.select('#post-193 > div.entry-content > div.content > div > div > div.col-lg-9 > div.items.list > div.item.book')
+
+  for i in range(1, len(items)+1):
+    a = wd.find_element_by_css_selector('div.item-data.d-flex > div.item-meta > div#item-{0}-location > div.item-loc-triggers > a'.format(i))
+    wd.execute_script("arguments[0].click();", a)
+  
+  html = wd.page_source
+  soup = BeautifulSoup(html, 'html.parser')
+  items = soup.select('#post-193 > div.entry-content > div.content > div > div > div.col-lg-9 > div.items.list > div.item.book')
   lst = []
 
   for item in items:
+    loan_available = 0
     dic = {}
     title = item.select_one('div.item-data.d-flex > div.item-meta > div.item-title').text
     author = item.select_one('div.item-data.d-flex > div.item-meta > div.item-author').text
@@ -34,6 +43,11 @@ def search_book_library(keyword):
     dic['title'] = title.strip()
     dic['author'] = author.strip()
     dic['link'] = link.strip()
+    cols = item.select("div.item-data.d-flex > div.item-meta > div.item-loc > div.item-loc-contents > div > div > table > tbody")
+    for col in cols:
+        status = col.select("tr > td")[4].text.split()
+        if status[1] == '대출가능': loan_available += 1
+    dic['available'] = loan_available
     lst.append(dic)
 
   return lst
@@ -58,7 +72,7 @@ def status_book_library(link):
         dic[col[0]] = cols.lstrip()
       else:
         dic[col[0]] = col[1]
-        if col[1] == '대출가능':
+        if col[0] == '도서상태' and col[1] != '대출중':
           break
     lst.append(dic)
 

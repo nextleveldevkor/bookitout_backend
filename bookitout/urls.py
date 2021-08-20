ad8947f63c857fa3ab2e503cf6e44611f905f226
Sangyups <feels4637@gmail.com>
2021-08-20 22:43:36 +0900

    chore: initial setting for service

--- bookitout/urls.py
+++ bookitout/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('rest_framework.urls')),
     path('libraries/', include('libraries.urls')),
+    path('', include('customer.urls'))
 ]

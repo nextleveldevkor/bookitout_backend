d44e569c1927a56c2559ff6886dd89bdaf02d0d9
Sangyups <feels4637@gmail.com>
2021-08-21 15:47:07 +0900

    feat: group exact same title within search

--- customer/urls.py
+++ customer/urls.py
@@ -4,6 +4,7 @@ from rest_framework import routers, urlpatterns
 
 router = routers.DefaultRouter()
 router.register('', views.BooksViewSet)
+router.register(r'search', views.BooksSearchViewSet)
 
 # urlpatterns = [
 #   path('products/', views.BooksView.as_view()),
--- customer/views.py
+++ customer/views.py
@@ -1,7 +1,8 @@
-from rest_framework import decorators, mixins
+from django.db.models.query import QuerySet
+from rest_framework import decorators, mixins, serializers
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from django.db.models import Count
-
 from customer.models import Books
 from customer.serializer import BooksSerializer, BooksSerializerGroup
 from customer.filter import BooksFilter
@@ -9,11 +10,12 @@ from customer.filter import BooksFilter
 class BooksViewSet(GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
   queryset = Books.objects.all().order_by('-id')
   serializer_class = BooksSerializer
+
+class BooksSearchViewSet(GenericViewSet, mixins.ListModelMixin):
+  queryset = Books.objects.values('title').annotate(count=Count('title'))
+  print(queryset)
+  serializer_class = BooksSerializerGroup
   filterset_class = BooksFilter
-  
-
-
-
 
 # newQuery = Books.objects.values('title').annotate(dcount=Count('title'))
 #     print(newQuery)

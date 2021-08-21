bdfe7aedc24b97fb985d4c589b998bc99ae9197a
Sangyups <feels4637@gmail.com>
2021-08-21 18:39:12 +0900

    fix: modify model

--- customer/models.py
+++ customer/models.py
@@ -4,7 +4,8 @@ from django.db.models import Q
 class Books(models.Model):
   title = models.CharField(max_length=100)
   author = models.CharField(max_length=64)
-  status = models.PositiveIntegerField()
+  edition = models.CharField(max_length=100)
+  condition = models.PositiveIntegerField()
   price = models.PositiveIntegerField()
   contact = models.TextField()
 
